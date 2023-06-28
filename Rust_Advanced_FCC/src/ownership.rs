015d3de1aafa30b1737c2f486c697752bc5985d2
Ata Tekeli <tekeliata2000@gmail.com>
2023-06-28 20:46:10 +0000

    28 Haz 2023 23:46

--- Rust_Advanced_FCC/src/ownership.rs
+++ Rust_Advanced_FCC/src/ownership.rs
@@ -1,11 +1,26 @@
 /*
 
 -Rust's ownership is unique and sets it apart from other programming languages
-
 -Set of rules that govern memory management
-
 -Rules are enforced at compile time
-
 -If any of the rules are violated, the program won't compile
+
+*/ 
+
+/*
+
+Scope:
+
+-Range within a program for which an item is valid
+
+Global Scope:
+
+-Accessible throughout the entire program
+
+Local Scope:
+
+-Accessible only within particular function or block of code
+-Not accessible outside of that function or block
+
 */ 
 
