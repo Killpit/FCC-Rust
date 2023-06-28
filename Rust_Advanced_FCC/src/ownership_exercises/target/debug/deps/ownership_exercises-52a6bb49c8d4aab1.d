519129e0abd37462c76e163c9fea086c179f1068
Ata Tekeli <tekeliata2000@gmail.com>
2023-06-28 21:39:32 +0000

    29 Haz 2023 00:39

---
+++ Rust_Advanced_FCC/src/ownership_exercises/target/debug/deps/ownership_exercises-52a6bb49c8d4aab1.d
@@ -0,0 +1,5 @@
+/Users/atatekeli/Documents/GitHub/FCC-Rust/Rust_Advanced_FCC/src/ownership_exercises/target/debug/deps/ownership_exercises-52a6bb49c8d4aab1.rmeta: src/main.rs
+
+/Users/atatekeli/Documents/GitHub/FCC-Rust/Rust_Advanced_FCC/src/ownership_exercises/target/debug/deps/ownership_exercises-52a6bb49c8d4aab1.d: src/main.rs
+
+src/main.rs:
---
+++ Rust_Advanced_FCC/src/ownership_exercises/target/debug/deps/ownership_exercises-72c8d00d92c165f3.d
@@ -0,0 +1,5 @@
+/Users/atatekeli/Documents/GitHub/FCC-Rust/Rust_Advanced_FCC/src/ownership_exercises/target/debug/deps/ownership_exercises-72c8d00d92c165f3.rmeta: src/main.rs
+
+/Users/atatekeli/Documents/GitHub/FCC-Rust/Rust_Advanced_FCC/src/ownership_exercises/target/debug/deps/ownership_exercises-72c8d00d92c165f3.d: src/main.rs
+
+src/main.rs:
