ff1893a12c18dd23aefcf74e667988b91b3cafa2
Ata Tekeli <tekeliata2000@gmail.com>
2023-06-28 20:41:09 +0000

    28 Haz 2023 23:41

---
+++ Rust_Advanced_FCC/src/ownership.rs
@@ -0,0 +1,11 @@
+/*
+
+-Rust's ownership is unique and sets it apart from other programming languages
+
+-Set of rules that govern memory management
+
+-Rules are enforced at compile time
+
+-If any of the rules are violated, the program won't compile
+*/ 
+
