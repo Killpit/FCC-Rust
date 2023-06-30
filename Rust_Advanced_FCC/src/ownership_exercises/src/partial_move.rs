e7aaabbe54c40c4d2abba87ae9adf707b7b30e03
Ata Tekeli <tekeliata2000@gmail.com>
2023-06-30 20:52:09 +0000

    30 Haz 2023 23:52

--- Rust_Advanced_FCC/src/ownership_exercises/src/partial_move.rs
+++ Rust_Advanced_FCC/src/ownership_exercises/src/partial_move.rs
@@ -4,4 +4,21 @@ time. Doing this will result in a partial move of the variable, which means that
 while other parts stay. In such a case, the parent variable cannot be used afterwards as a whole, however the parts that
 are only referenced (and not moved) can still be used */
 
-pub fn run() {}
+pub fn run() {
+    #[derive(Debug)]
+    struct Person {
+        name: String,
+        age: Box<u8>,
+    }
+
+    let person: Person = Person {
+        name: String::from("Alice"),
+        age: Box::new(20),
+    };
+
+    //'name' is moved out of person, but 'age' is referenced
+    let Person {name, ref age} = person;
+
+    println!("The person's age is {}", age);
+    println!("The person's name is {}", name);
+}
