5fe35caa2a11c7517e0189bd6937351d32c93865
Ata Tekeli <tekeliata2000@gmail.com>
2023-06-30 22:07:58 +0000

    1 Tem 2023 01:07

--- Rust_Advanced_FCC/src/borrowing_exercises/src/ref.rs
+++ Rust_Advanced_FCC/src/borrowing_exercises/src/ref.rs
@@ -1,3 +1,12 @@
 pub fn run() {
-    
+    let c: char = '中';
+
+    let r1: &char = &c;
+    let __ r2 = c;
+
+    assert_eq!(*r1, *r2);
+
+    assert_eq!(get_addr(r1), get_addr(r2));
+
+    println!("Success");
 }
