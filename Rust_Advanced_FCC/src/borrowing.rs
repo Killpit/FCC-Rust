0e8407310274f2dbfd4d69960c2599294ad22225
Ata Tekeli <tekeliata2000@gmail.com>
2023-06-30 21:12:01 +0000

    1 Tem 2023 00:12

--- Rust_Advanced_FCC/src/borrowing.rs
+++ Rust_Advanced_FCC/src/borrowing.rs
@@ -1,2 +1,9 @@
 /*
- */
+Borrowing:
+
+-Way of temporarily access data without taking ownership of it.
+-When borrowing, you're taking a reference (pointer) to the data, not the data itself.
+-Prevention of dangling pointers and data races 
+-Data can be borrowed immutably and mutably
+-There are certain rules when borrowing which we have to comply with, otherwise, the program won't compile */
+
