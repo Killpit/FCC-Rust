7514acc09f9365ef0246eae4a35fc11698460267
Ata Tekeli <tekeliata2000@gmail.com>
2023-06-30 21:58:02 +0000

    1 Tem 2023 00:58

---
+++ Rust_Advanced_FCC/src/borrowing_exercises/src/borrowing_exercise_four.rs
@@ -0,0 +1,11 @@
+pub fn run() {
+    let mut s: String = String::from("hello, ");
+
+    push_str(&mut s);
+
+    println!("Success");
+}
+
+fn push_str(s: &mut String) {
+    s.push_str("world")
+}
--- Rust_Advanced_FCC/src/borrowing_exercises/src/main.rs
+++ Rust_Advanced_FCC/src/borrowing_exercises/src/main.rs
@@ -1,9 +1,11 @@
 //mod borrowing_exercise_one;
 //mod borrowing_exercise_two;
-mod borrowing_exercise_three;
+//mod borrowing_exercise_three;
+mod borrowing_exercise_four;
 
 fn main() {
     //borrowing_exercise_one::run();
     //borrowing_exercise_two::run();
-    borrowing_exercise_three::run();
+    //borrowing_exercise_three::run();
+    borrowing_exercise_four::run();
 }
