01c91a1e2f62e433de88d1793fb1c5d713e49160
Ata Tekeli <tekeliata2000@gmail.com>
2023-06-30 21:40:13 +0000

    1 Tem 2023 00:40

--- Rust_Advanced_FCC/src/borrowing_exercises/src/main.rs
+++ Rust_Advanced_FCC/src/borrowing_exercises/src/main.rs
@@ -1,5 +1,7 @@
-mod borrowing_exercise_one;
+//mod borrowing_exercise_one;
+mod borrow_exercise_two;
 
 fn main() {
-    borrowing_exercise_one::run();
+    //borrowing_exercise_one::run();
+    borrow_exercise_two::run();
 }
