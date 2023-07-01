2e2fd9f2040f452bc1fa9dcdf66905cf6ec0b585
Ata Tekeli <tekeliata2000@gmail.com>
2023-07-01 17:22:25 +0000

    1 Tem 2023 20:22

--- Rust_Advanced_FCC/src/borrowing_exercises/src/borrowing_rules.rs
+++ Rust_Advanced_FCC/src/borrowing_exercises/src/borrowing_rules.rs
@@ -1,8 +1,10 @@
 pub fn run() {
     let mut s: String = String::from("hello");
 
-    let r1 = &mut s;
-    let r2 = &mut s;
+    let r1 = &s;
+    let r2 = &s;
 
     println!("{}, {}", r1, r2);
+
+    println!
 }
