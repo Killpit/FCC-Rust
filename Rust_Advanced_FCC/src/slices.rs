7c33ce39002785dd3bf862c7e43c15eade147ff3
Ata Tekeli <tekeliata2000@gmail.com>
2023-07-01 18:04:35 +0000

    1 Tem 2023 21:04

---
+++ Rust_Advanced_FCC/src/slices.rs
@@ -0,0 +1,7 @@
+/*
+Slice
+
+-Reference to contiguous sequence of elements in a collection
+-Provide a way to borrow path of a collection without taking ownership of the entire collection
+-Can be created from arrays, vectors, Strings and other collections implementing the Deref trait */
+
