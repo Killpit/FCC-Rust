867da8e0636e2b9a745f6ada3cee5654c7a9be7a
Ata Tekeli <tekeliata2000@gmail.com>
2023-07-01 18:13:33 +0000

    1 Tem 2023 21:13

--- Rust_Advanced_FCC/src/slices.rs
+++ Rust_Advanced_FCC/src/slices.rs
@@ -5,3 +5,6 @@ Slice
 -Provide a way to borrow path of a collection without taking ownership of the entire collection
 -Can be created from arrays, vectors, Strings and other collections implementing the Deref trait */
 
+pub fn run() {
+    
+}
