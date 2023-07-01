b1f96e7ef2215cee48e60e5a356c2e3b7c4d4e39
Ata Tekeli <tekeliata2000@gmail.com>
2023-07-01 17:55:06 +0000

    1 Tem 2023 20:55

--- Rust_Advanced_FCC/src/borrowing_exercises/src/nll.rs
+++ Rust_Advanced_FCC/src/borrowing_exercises/src/nll.rs
@@ -1,7 +1,7 @@
 pub fn run() {
     lat mut s: String = String::from("hello, ");
 
-    let r1 = &mut s;
+    let r1: &mut String = &mut s;
     r1.push_str("world");
     let r2 = &mut s;
     r2.push_str("!");
