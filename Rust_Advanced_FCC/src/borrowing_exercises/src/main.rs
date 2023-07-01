ace4ec28d8d221e603cc0826093dd70f1ed1125c
Ata Tekeli <tekeliata2000@gmail.com>
2023-07-01 17:33:39 +0000

    1 Tem 2023 20:33

--- Rust_Advanced_FCC/src/borrowing_exercises/src/main.rs
+++ Rust_Advanced_FCC/src/borrowing_exercises/src/main.rs
@@ -1,19 +1,19 @@
-mod borrowing_exercise_one;
-mod borrowing_exercise_two;
-mod borrowing_exercise_three;
-mod borrowing_exercise_four;
-mod borrowing_exercise_five;
-mod reference;
-mod borrowing_rules;
-mod mutability;
+//mod borrowing_exercise_one;
+//mod borrowing_exercise_two;
+//mod borrowing_exercise_three;
+//mod borrowing_exercise_four;
+//mod borrowing_exercise_five;
+//mod reference;
+//mod borrowing_rules;
+//mod mutability;
 
 fn main() {
-    borrowing_exercise_one::run();
-    borrowing_exercise_two::run();
-    borrowing_exercise_three::run();
-    borrowing_exercise_four::run();
-    borrowing_exercise_five::run();
-    reference::run();
-    borrowing_rules::run();
+    //borrowing_exercise_one::run();
+    //borrowing_exercise_two::run();
+    //borrowing_exercise_three::run();
+    //borrowing_exercise_four::run();
+    //borrowing_exercise_five::run();
+    //reference::run();
+    //borrowing_rules::run();
     mutability::run();
 }
