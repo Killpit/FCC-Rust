30546e0ce75e5da3ac82a72f7ec7e6d2ecc8266d
Ata Tekeli <tekeliata2000@gmail.com>
2023-07-01 18:21:45 +0000

    1 Tem 2023 21:21

--- Rust_Advanced_FCC/src/slice_exercises/src/slice_exercise_one.rs
+++ Rust_Advanced_FCC/src/slice_exercises/src/slice_exercise_one.rs
@@ -2,7 +2,7 @@ pub fn run() {
     let arr: [i32; 3] = [1, 2, 3];
     let s1: &[i32] = &arr[0..2];
 
-    let s2: str = "hello, world" as str;
+    let s2: &str = "hello, world";
 
     println!("Success");
 }
