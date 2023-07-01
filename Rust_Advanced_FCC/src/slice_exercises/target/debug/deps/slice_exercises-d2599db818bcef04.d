2add6941dd4a6e5e4d7a4dffa765d012943aefa6
Ata Tekeli <tekeliata2000@gmail.com>
2023-07-01 18:23:01 +0000

    1 Tem 2023 21:23

---
+++ Rust_Advanced_FCC/src/slice_exercises/target/debug/deps/slice_exercises-d2599db818bcef04.d
@@ -0,0 +1,6 @@
+/Users/atatekeli/Documents/GitHub/FCC-Rust/Rust_Advanced_FCC/src/slice_exercises/target/debug/deps/slice_exercises-d2599db818bcef04: src/main.rs src/slice_exercise_one.rs
+
+/Users/atatekeli/Documents/GitHub/FCC-Rust/Rust_Advanced_FCC/src/slice_exercises/target/debug/deps/slice_exercises-d2599db818bcef04.d: src/main.rs src/slice_exercise_one.rs
+
+src/main.rs:
+src/slice_exercise_one.rs:
