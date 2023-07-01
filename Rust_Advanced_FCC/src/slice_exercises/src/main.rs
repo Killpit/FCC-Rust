0d12a6740c3b69f082fedcc0796fbbca61df908d
Ata Tekeli <tekeliata2000@gmail.com>
2023-07-01 18:35:19 +0000

    1 Tem 2023 21:35

--- Rust_Advanced_FCC/src/slice_exercises/src/main.rs
+++ Rust_Advanced_FCC/src/slice_exercises/src/main.rs
@@ -1,7 +1,9 @@
 //mod slice_exercise_one;
-mod slice_reference;
+//mod slice_reference;
+mod slice_exercise_three;
 
 fn main() {
     //slice_exercise_one::run();
-    slice_reference::run();
+    //slice_reference::run();
+    slice_exercise_three::run();
 }
