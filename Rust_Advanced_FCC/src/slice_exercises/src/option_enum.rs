a85a449b860d5a88ea9ac62bbe35e1c9b6e853a5
Ata Tekeli <tekeliata2000@gmail.com>
2023-07-08 21:19:49 +0000

    9 Tem 2023 00:19

--- Rust_Advanced_FCC/src/slice_exercises/src/option_enum.rs
+++ Rust_Advanced_FCC/src/slice_exercises/src/option_enum.rs
@@ -20,3 +20,9 @@ pub fn run() {
     panic!("NEVER LET THIS RUN! ");
 }
 
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
