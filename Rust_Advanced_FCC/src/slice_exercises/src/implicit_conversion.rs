08ca3aa65838e11bfd5a91f7e5bef44c76a5284d
Ata Tekeli <tekeliata2000@gmail.com>
2023-07-08 20:51:26 +0000

    8 Tem 2023 23:51

--- Rust_Advanced_FCC/src/slice_exercises/src/implicit_conversion.rs
+++ Rust_Advanced_FCC/src/slice_exercises/src/implicit_conversion.rs
@@ -1,3 +1,13 @@
 pub fn run() {
-    
+    let mut s: String = String::from("hello world");
+
+    let word = first_word(&s);
+
+    s.clear();
+
+    println!("the first word is: {}", word);
+}
+
+fn first_word(s: &str) -> &str {
+    &s[..1]
 }
