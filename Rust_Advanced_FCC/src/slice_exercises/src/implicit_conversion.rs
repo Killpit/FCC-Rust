1e73bee1e9f8c79cda831c2709c4714e015ced7f
Ata Tekeli <tekeliata2000@gmail.com>
2023-07-08 21:03:14 +0000

    9 Tem 2023 00:03

--- Rust_Advanced_FCC/src/slice_exercises/src/implicit_conversion.rs
+++ Rust_Advanced_FCC/src/slice_exercises/src/implicit_conversion.rs
@@ -2,10 +2,9 @@ pub fn run() {
     let mut s: String = String::from("hello world");
 
     let word: &str = first_word(&s);
+    println!("the first word is: {}", word);
 
     s.clear();
-
-    println!("the first word is: {}", word);
 }
 
 fn first_word(s: &str) -> &str {
--- Rust_Advanced_FCC/src/slice_exercises/target/debug/deps/slice_exercises-d2599db818bcef04.d
+++ Rust_Advanced_FCC/src/slice_exercises/target/debug/deps/slice_exercises-d2599db818bcef04.d
@@ -1,6 +1,6 @@
-/Users/atatekeli/Documents/GitHub/FCC-Rust/Rust_Advanced_FCC/src/slice_exercises/target/debug/deps/slice_exercises-d2599db818bcef04: src/main.rs src/exercise_five.rs
+/Users/atatekeli/Documents/GitHub/FCC-Rust/Rust_Advanced_FCC/src/slice_exercises/target/debug/deps/slice_exercises-d2599db818bcef04: src/main.rs src/implicit_conversion.rs
 
-/Users/atatekeli/Documents/GitHub/FCC-Rust/Rust_Advanced_FCC/src/slice_exercises/target/debug/deps/slice_exercises-d2599db818bcef04.d: src/main.rs src/exercise_five.rs
+/Users/atatekeli/Documents/GitHub/FCC-Rust/Rust_Advanced_FCC/src/slice_exercises/target/debug/deps/slice_exercises-d2599db818bcef04.d: src/main.rs src/implicit_conversion.rs
 
 src/main.rs:
-src/exercise_five.rs:
+src/implicit_conversion.rs:
