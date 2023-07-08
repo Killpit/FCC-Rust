36db94393de8528bf69ab5dd74cf1deb0ea1d65f
Ata Tekeli <tekeliata2000@gmail.com>
2023-07-08 21:10:23 +0000

    9 Tem 2023 00:10

--- Rust_Advanced_FCC/src/slice_exercises/src/option_enum.rs
+++ Rust_Advanced_FCC/src/slice_exercises/src/option_enum.rs
@@ -1,3 +1,11 @@
+/*
+-Option is an enum that represents a value that may or may not be present.
+
+-Known in other languages as "null", referring to the absence of value
+
+-Used to handle cases where a function or method might fail to return a value
+*/
+
 pub fn run() {
     
 }
