b7c27b05c32d895e0b0d1d35dd68cd2057605ff1
Ata Tekeli <tekeliata2000@gmail.com>
2023-07-08 21:08:51 +0000

    9 Tem 2023 00:08

--- Rust_Advanced_FCC/src/slice_exercises/src/main.rs
+++ Rust_Advanced_FCC/src/slice_exercises/src/main.rs
@@ -3,7 +3,8 @@
 //mod slice_exercise_three;
 //mod string_slices;
 //mod exercise_five;
-mod implicit_conversion;
+//mod implicit_conversion;
+mod option_enum;
 
 fn main() {
     //slice_exercise_one::run();
@@ -11,5 +12,6 @@ fn main() {
     //slice_exercise_three::run();
     //string_slices::run();
     //exercise_five::run();
-    implicit_conversion::run();
+    //implicit_conversion::run();
+    option_enum::run();
 }
