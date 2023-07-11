176474cb5a63d9403cd0683967ee53650eea36af
Ata Tekeli <tekeliata2000@gmail.com>
2023-07-11 21:47:22 +0000

    12 Tem 2023 00:47

--- Rust_Advanced_FCC/src/generics_exercises/src/functions.rs
+++ Rust_Advanced_FCC/src/generics_exercises/src/functions.rs
@@ -11,9 +11,9 @@ fn gen_spec_i32(_s: SGen<i32>) {}
 fn generic<T>(_s: SGen<T>) {}
 
 pub fn run() {
-    reg_fn(__);
-    gen_spec_t(__);
-    gen_spec_i32(__);
+    reg_fn(S(A));
+    gen_spec_t(SGen(A));
+    gen_spec_i32(SGen(7));
 
     generic::<char>(__);
 
