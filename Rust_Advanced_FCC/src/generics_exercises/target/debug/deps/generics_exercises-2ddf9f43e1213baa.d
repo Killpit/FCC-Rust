cce9cb247e9cf4a30ba01e8414efb39a1df1b177
Ata Tekeli <tekeliata2000@gmail.com>
2023-07-11 22:02:00 +0000

    12 Tem 2023 01:02

--- Rust_Advanced_FCC/src/generics_exercises/target/debug/deps/generics_exercises-2ddf9f43e1213baa.d
+++ Rust_Advanced_FCC/src/generics_exercises/target/debug/deps/generics_exercises-2ddf9f43e1213baa.d
@@ -1,6 +1,6 @@
-/Users/atatekeli/Documents/GitHub/FCC-Rust/Rust_Advanced_FCC/src/generics_exercises/target/debug/deps/generics_exercises-2ddf9f43e1213baa: src/main.rs src/sum.rs
+/Users/atatekeli/Documents/GitHub/FCC-Rust/Rust_Advanced_FCC/src/generics_exercises/target/debug/deps/generics_exercises-2ddf9f43e1213baa: src/main.rs src/generics_exercise_two.rs
 
-/Users/atatekeli/Documents/GitHub/FCC-Rust/Rust_Advanced_FCC/src/generics_exercises/target/debug/deps/generics_exercises-2ddf9f43e1213baa.d: src/main.rs src/sum.rs
+/Users/atatekeli/Documents/GitHub/FCC-Rust/Rust_Advanced_FCC/src/generics_exercises/target/debug/deps/generics_exercises-2ddf9f43e1213baa.d: src/main.rs src/generics_exercise_two.rs
 
 src/main.rs:
-src/sum.rs:
+src/generics_exercise_two.rs:
