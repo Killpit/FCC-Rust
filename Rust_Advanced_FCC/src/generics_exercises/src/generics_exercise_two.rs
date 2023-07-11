7834613b46935f957b66c8a3085ec4d50af851c3
Ata Tekeli <tekeliata2000@gmail.com>
2023-07-11 22:01:05 +0000

    12 Tem 2023 01:01

--- Rust_Advanced_FCC/src/generics_exercises/src/generics_exercise_two.rs
+++ Rust_Advanced_FCC/src/generics_exercises/src/generics_exercise_two.rs
@@ -4,8 +4,8 @@ struct Point<T> {
 }
 
 pub fn run() {
-    let integer: Point = Point {x: 5, y: 10};
-    let float: Point = Point {x: 1.0, y: 4.0};
+    let integer: Point<i32> = Point {x: 5, y: 10};
+    let float: Point<f64> = Point {x: 1.0, y: 4.0};
 
     println!("Success");
 }
