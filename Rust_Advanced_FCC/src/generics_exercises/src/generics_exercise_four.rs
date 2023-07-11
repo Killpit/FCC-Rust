e220f5551d2c0b7276faba6bdaf2c04ac38675e0
Ata Tekeli <tekeliata2000@gmail.com>
2023-07-11 22:14:53 +0000

    12 Tem 2023 01:14

--- Rust_Advanced_FCC/src/generics_exercises/src/generics_exercise_four.rs
+++ Rust_Advanced_FCC/src/generics_exercises/src/generics_exercise_four.rs
@@ -2,14 +2,15 @@ struct Val<T> {
     val: T,
 }
 
-impl Val {
+impl<T> Val<T> {
     fn value(&self) -> &f64 {
         &self.val
     }
 }
 
 pub fn run() {
-    let x = Val{val: 3.0};
-    let y = Val{val: "hello".to_string()};
+    let x: Val<f64> = Val{val: 3.0};
+    let y: Val<String> = Val{val: "hello".to_string()};
+
     println!("{}, {}", x.value(), y.value());
 }
